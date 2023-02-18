0122095a368a30b7f9f6ecbd5432b42d0f731db1
denny <denny.chang@invos.com.tw>
2023-02-18 08:31:14 +0800

    chore: make doc folder a storybook folder

---
+++ apps/docs/stories/DesktopApp.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { DesktopApp } from "ui";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/DesktopApp",
+  component: DesktopApp,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof DesktopApp>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof DesktopApp> = (args) => (
+  <DesktopApp {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {};
