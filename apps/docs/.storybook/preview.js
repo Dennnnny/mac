928811a232c84817666eb15aaf59fb8d5e028353
denny <denny.chang@invos.com.tw>
2023-02-27 21:41:43 +0800

    feature: reset css

--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -1,3 +1,5 @@
+import "!style-loader!css-loader!postcss-loader!ui/config/reset.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- apps/mac/pages/_app.tsx
+++ apps/mac/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import { createActorContext } from "@xstate/react";
 import { desktopMachine } from "utils/machine";
-
+import "ui/config/reset.css";
 export const DesktopContext = createActorContext(desktopMachine);
 
 function App({ Component, pageProps }: AppProps) {
